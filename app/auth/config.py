50a9a60ac75dd5bc3e5f1efa9b16f6848420df3e
nikit <nikita.fedotov222@gmail.com>
2025-08-01 14:16:14 +0300

    feat: implement authentication module with JWT strategy and user management

---
+++ app/auth/config.py
@@ -0,0 +1,22 @@
+from fastapi_users.authentication import (
+    JWTStrategy,
+    AuthenticationBackend,
+    CookieTransport
+)
+
+
+SECRET = "VERY_SECRET"
+
+
+cookie_transport = CookieTransport(cookie_name="binauth", cookie_max_age=3600*24*30)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600*24*30)
+
+
+jwt_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
