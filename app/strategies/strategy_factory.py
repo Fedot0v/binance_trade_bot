109a0c200a7f3105c1b974545698d22d6ede07d3
nikit <nikita.fedotov222@gmail.com>
2025-08-08 15:47:23 +0300

    fix hardcode novichok strategy to strategy_factory

--- app/strategies/strategy_factory.py
+++ app/strategies/strategy_factory.py
@@ -12,7 +12,7 @@ def get_strategy_class_by_name(
     name: str,
     params: StrategyParameters
 ) -> BaseStrategy:
-    strategy_cls = STRATEGY_REGISTRY.get(name)
+    strategy_cls = STRATEGY_REGISTRY.get(name.lower())
     if not strategy_cls:
         raise ValueError(f"Strategy '{name}' not found")
     return strategy_cls(params)
